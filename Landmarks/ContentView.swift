aa2ac082e26b06457e41f49021e935aca1af8eef
jeremiasdsa <jeremias.serafim.araujo@gmail.com>
2023-09-20 13:15:02 -0300

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,55 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Jeremias Serafim on 20/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView2: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "person.fill")
+                .imageScale(.large)
+                
+            Text("Hello, world!")
+                .font(.title)
+                
+        }
+        .padding()
+    }
+}
+
+struct MyView: View {
+    var body: some View {
+        VStack (alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Part")
+                    .font(.subheadline)
+                Spacer()
+                Text("Jeremias")
+                    .font(.subheadline)
+                    .foregroundColor(.green)
+            }
+        }.padding()
+    }
+}
+
+#Preview {
+    MyView()
+}
---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Jeremias Serafim on 20/09/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
